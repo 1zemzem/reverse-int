5d67960c25f31bf12f58f5f4c3a8c89c0a61908e
1zemzem <1zemliakova@gmail.com>
2023-01-10 16:19:31 +0300

    solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    const absoluteN = Math.abs(n);
+    const strN = String(absoluteN);
+    const reverseStr = strN.split("").reverse().join("");
+    return Number(reverseStr);
+};
